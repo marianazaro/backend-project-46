2e3cc58ee8362cdb869e7bd2766a40983543e1e4
maria <marianazarova892@gmail.com>
2022-12-06 15:23:09 +0300

    add gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+// eslint-disable-next-line import/extensions
+import genDiff from '../src/index.js';
 
 const program = new Command();
 program
   .name('gendiff')
-  .version('0')
+  .version('0.1.0')
   .description('Compares two configuration files and shows a differense.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, options) => {
+    // eslint-disable-next-line no-console
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
 program.parse();
---
+++ src/parsers.js
@@ -0,0 +1,13 @@
+import yaml from 'js-yaml';
+
+export default (format, data) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Unknown format: ${format}`);
+  }
+};
